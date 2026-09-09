a0acb6fac1a031f15b2e5bb3946867ffe79345b5
João Victor Pereira de Lima <joaovictorjoaovictorpereira123@gmail.com>
2026-09-09 20:27:19 -0300

    feat: adiciona erro de recurso nao encontrado

---
+++ src/main/java/com/joaovictor/exception/RecursoNaoEncontradoException.java
@@ -0,0 +1,8 @@
+package com.joaovictor.exception;
+
+public class RecursoNaoEncontradoException extends RuntimeException {
+
+    public RecursoNaoEncontradoException(String message) {
+        super(message);
+    }
+}
