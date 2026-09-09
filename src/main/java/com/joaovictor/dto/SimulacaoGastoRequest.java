5d904125a436ce7d650c459db39748744a731c64
João Victor Pereira de Lima <joaovictorjoaovictorpereira123@gmail.com>
2026-09-09 15:37:36 -0300

    feat: adiciona dto para simulacao de gasto

---
+++ src/main/java/com/joaovictor/dto/SimulacaoGastoRequest.java
@@ -0,0 +1,16 @@
+package com.joaovictor.dto;
+
+import java.math.BigDecimal;
+
+public class SimulacaoGastoRequest {
+
+    private BigDecimal valor;
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+}
