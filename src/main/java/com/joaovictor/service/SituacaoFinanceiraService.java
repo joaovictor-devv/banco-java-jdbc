6ef2332d330d6afac4dea20f9a896c5f146d99ee
João Victor Pereira de Lima <joaovictorjoaovictorpereira123@gmail.com>
2026-09-09 17:19:17 -0300

    refactor: remove historico da situacao financeira

--- src/main/java/com/joaovictor/service/SituacaoFinanceiraService.java
+++ src/main/java/com/joaovictor/service/SituacaoFinanceiraService.java
@@ -1,36 +1,30 @@
 package com.joaovictor.service;
 
 import com.joaovictor.model.CapacidadeFinanceira;
-import com.joaovictor.model.ResumoFinanceiro;
 import com.joaovictor.model.SituacaoFinanceira;
 
 public class SituacaoFinanceiraService {
 
     private final MotorFinanceiroService motorFinanceiroService;
-    private final AnaliseFinanceiraService analiseFinanceiraService;
 
     public SituacaoFinanceiraService() {
         this.motorFinanceiroService = new MotorFinanceiroService();
-        this.analiseFinanceiraService = new AnaliseFinanceiraService();
     }
 
     public SituacaoFinanceira analisar() {
         CapacidadeFinanceira capacidade = motorFinanceiroService.calcularCapacidade();
-        ResumoFinanceiro resumo = analiseFinanceiraService.gerarResumoDoMesAtual();
 
         return new SituacaoFinanceira(
                 capacidade.getRendaMensal(),
-                capacidade.getRendaExtra(),
-                capacidade.getRendaTotal(),
-                capacidade.getDespesasPlanejadas(),
+                capacidade.getGastosMensais(),
                 capacidade.getReservaPlanejada(),
                 capacidade.getMargemAntesMetas(),
                 capacidade.getComprometimentoMensalMetas(),
                 capacidade.getMargemAposMetas(),
+                capacidade.getCapacidadeGastoMensal(),
+                capacidade.getCapacidadeGastoImediato(),
                 capacidade.getSaldoAtual(),
-                resumo.getSaldoMes(),
-                resumo.getTotalEntradas(),
-                resumo.getTotalSaidas(),
+                capacidade.getPercentualRendaComprometida(),
                 capacidade.getClassificacao()
         );
     }
