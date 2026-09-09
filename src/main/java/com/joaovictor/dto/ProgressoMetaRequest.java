c7cce5e01f4bb37573b2a87fdcfe189bb3ab3526
João Victor Pereira de Lima <joaovictorjoaovictorpereira123@gmail.com>
2026-09-09 16:52:12 -0300

    feat: adiciona request de progresso da meta

---
+++ src/main/java/com/joaovictor/dto/ProgressoMetaRequest.java
@@ -0,0 +1,11 @@
+package com.joaovictor.dto;
+
+import java.math.BigDecimal;
+
+public class ProgressoMetaRequest {
+
+    private BigDecimal valorAtual;
+
+    public BigDecimal getValorAtual() { return valorAtual; }
+    public void setValorAtual(BigDecimal valorAtual) { this.valorAtual = valorAtual; }
+}
