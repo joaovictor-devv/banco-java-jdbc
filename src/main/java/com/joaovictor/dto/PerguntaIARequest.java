ffc2d5e3e457c6f731220b3aad2259a96261428b
João Victor Pereira de Lima <joaovictorjoaovictorpereira123@gmail.com>
2026-09-09 14:21:32 -0300

    feat: adicionar request para perguntas da IA

---
+++ src/main/java/com/joaovictor/dto/PerguntaIARequest.java
@@ -0,0 +1,17 @@
+package com.joaovictor.dto;
+
+public class PerguntaIARequest {
+
+    private String pergunta;
+
+    public PerguntaIARequest() {
+    }
+
+    public String getPergunta() {
+        return pergunta;
+    }
+
+    public void setPergunta(String pergunta) {
+        this.pergunta = pergunta;
+    }
+}
