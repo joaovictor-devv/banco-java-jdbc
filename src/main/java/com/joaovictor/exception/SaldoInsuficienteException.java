5b65c1607bcd8f6744e86160a97f26e67ec5be4f
João Victor Pereira de Lima <joaovictorjoaovictorpereira123@gmail.com>
2026-09-09 17:29:28 -0300

    refactor: remove excecao de saldo usada por transacoes

--- src/main/java/com/joaovictor/exception/SaldoInsuficienteException.java
+++
@@ -1,7 +0,0 @@
-package com.joaovictor.exception;
-
-public class SaldoInsuficienteException extends RuntimeException {
-    public SaldoInsuficienteException(String mensagem) {
-        super(mensagem);
-    }
-}
