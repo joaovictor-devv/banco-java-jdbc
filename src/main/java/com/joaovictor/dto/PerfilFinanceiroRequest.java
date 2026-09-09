b4f629cc89b1310b39807c177e2f8b215cb85a21
João Victor Pereira de Lima <joaovictorjoaovictorpereira123@gmail.com>
2026-09-09 14:51:04 -0300

    feat: receber saldo atual no perfil

--- src/main/java/com/joaovictor/dto/PerfilFinanceiroRequest.java
+++ src/main/java/com/joaovictor/dto/PerfilFinanceiroRequest.java
@@ -15,92 +15,30 @@ public class PerfilFinanceiroRequest {
     private BigDecimal outrasDespesas;
     private BigDecimal valorPlanejadoGuardar;
     private String objetivoPrincipal;
+    private BigDecimal saldoAtual;
 
-    public BigDecimal getRendaMensal() {
-        return rendaMensal;
-    }
-
-    public void setRendaMensal(BigDecimal rendaMensal) {
-        this.rendaMensal = rendaMensal;
-    }
-
-    public BigDecimal getRendaExtra() {
-        return rendaExtra;
-    }
-
-    public void setRendaExtra(BigDecimal rendaExtra) {
-        this.rendaExtra = rendaExtra;
-    }
-
-    public BigDecimal getGastoMoradia() {
-        return gastoMoradia;
-    }
-
-    public void setGastoMoradia(BigDecimal gastoMoradia) {
-        this.gastoMoradia = gastoMoradia;
-    }
-
-    public BigDecimal getGastoAgua() {
-        return gastoAgua;
-    }
-
-    public void setGastoAgua(BigDecimal gastoAgua) {
-        this.gastoAgua = gastoAgua;
-    }
-
-    public BigDecimal getGastoEnergia() {
-        return gastoEnergia;
-    }
-
-    public void setGastoEnergia(BigDecimal gastoEnergia) {
-        this.gastoEnergia = gastoEnergia;
-    }
-
-    public BigDecimal getGastoInternet() {
-        return gastoInternet;
-    }
-
-    public void setGastoInternet(BigDecimal gastoInternet) {
-        this.gastoInternet = gastoInternet;
-    }
-
-    public BigDecimal getGastoTransporte() {
-        return gastoTransporte;
-    }
-
-    public void setGastoTransporte(BigDecimal gastoTransporte) {
-        this.gastoTransporte = gastoTransporte;
-    }
-
-    public BigDecimal getGastoAlimentacao() {
-        return gastoAlimentacao;
-    }
-
-    public void setGastoAlimentacao(BigDecimal gastoAlimentacao) {
-        this.gastoAlimentacao = gastoAlimentacao;
-    }
-
-    public BigDecimal getOutrasDespesas() {
-        return outrasDespesas;
-    }
-
-    public void setOutrasDespesas(BigDecimal outrasDespesas) {
-        this.outrasDespesas = outrasDespesas;
-    }
-
-    public BigDecimal getValorPlanejadoGuardar() {
-        return valorPlanejadoGuardar;
-    }
-
-    public void setValorPlanejadoGuardar(BigDecimal valorPlanejadoGuardar) {
-        this.valorPlanejadoGuardar = valorPlanejadoGuardar;
-    }
-
-    public String getObjetivoPrincipal() {
-        return objetivoPrincipal;
-    }
-
-    public void setObjetivoPrincipal(String objetivoPrincipal) {
-        this.objetivoPrincipal = objetivoPrincipal;
-    }
+    public BigDecimal getRendaMensal() { return rendaMensal; }
+    public void setRendaMensal(BigDecimal rendaMensal) { this.rendaMensal = rendaMensal; }
+    public BigDecimal getRendaExtra() { return rendaExtra; }
+    public void setRendaExtra(BigDecimal rendaExtra) { this.rendaExtra = rendaExtra; }
+    public BigDecimal getGastoMoradia() { return gastoMoradia; }
+    public void setGastoMoradia(BigDecimal gastoMoradia) { this.gastoMoradia = gastoMoradia; }
+    public BigDecimal getGastoAgua() { return gastoAgua; }
+    public void setGastoAgua(BigDecimal gastoAgua) { this.gastoAgua = gastoAgua; }
+    public BigDecimal getGastoEnergia() { return gastoEnergia; }
+    public void setGastoEnergia(BigDecimal gastoEnergia) { this.gastoEnergia = gastoEnergia; }
+    public BigDecimal getGastoInternet() { return gastoInternet; }
+    public void setGastoInternet(BigDecimal gastoInternet) { this.gastoInternet = gastoInternet; }
+    public BigDecimal getGastoTransporte() { return gastoTransporte; }
+    public void setGastoTransporte(BigDecimal gastoTransporte) { this.gastoTransporte = gastoTransporte; }
+    public BigDecimal getGastoAlimentacao() { return gastoAlimentacao; }
+    public void setGastoAlimentacao(BigDecimal gastoAlimentacao) { this.gastoAlimentacao = gastoAlimentacao; }
+    public BigDecimal getOutrasDespesas() { return outrasDespesas; }
+    public void setOutrasDespesas(BigDecimal outrasDespesas) { this.outrasDespesas = outrasDespesas; }
+    public BigDecimal getValorPlanejadoGuardar() { return valorPlanejadoGuardar; }
+    public void setValorPlanejadoGuardar(BigDecimal valorPlanejadoGuardar) { this.valorPlanejadoGuardar = valorPlanejadoGuardar; }
+    public String getObjetivoPrincipal() { return objetivoPrincipal; }
+    public void setObjetivoPrincipal(String objetivoPrincipal) { this.objetivoPrincipal = objetivoPrincipal; }
+    public BigDecimal getSaldoAtual() { return saldoAtual; }
+    public void setSaldoAtual(BigDecimal saldoAtual) { this.saldoAtual = saldoAtual; }
 }
