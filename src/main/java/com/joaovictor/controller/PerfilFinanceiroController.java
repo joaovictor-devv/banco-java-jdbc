8a29a91b8b22f2fa8eaab3da76e41fda1b732ffe
João Victor Pereira de Lima <joaovictorjoaovictorpereira123@gmail.com>
2026-09-09 14:15:15 -0300

    feat: expor situação financeira pelo perfil

--- src/main/java/com/joaovictor/controller/PerfilFinanceiroController.java
+++ src/main/java/com/joaovictor/controller/PerfilFinanceiroController.java
@@ -3,7 +3,9 @@ package com.joaovictor.controller;
 import com.joaovictor.dto.ApiResponse;
 import com.joaovictor.dto.PerfilFinanceiroRequest;
 import com.joaovictor.model.PerfilFinanceiro;
+import com.joaovictor.model.SituacaoFinanceira;
 import com.joaovictor.service.PerfilFinanceiroService;
+import com.joaovictor.service.SituacaoFinanceiraService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 public class PerfilFinanceiroController {
 
     private final PerfilFinanceiroService service = new PerfilFinanceiroService();
+    private final SituacaoFinanceiraService situacaoService = new SituacaoFinanceiraService();
 
     @PostMapping
     public ResponseEntity<ApiResponse> cadastrar(@RequestBody PerfilFinanceiroRequest request) {
@@ -42,6 +45,11 @@ public class PerfilFinanceiroController {
     @GetMapping("/{id}")
     public ResponseEntity<PerfilFinanceiro> buscarPorId(@PathVariable long id) {
         return ResponseEntity.ok(service.buscarPorId(id));
+    }
+
+    @GetMapping("/situacao")
+    public ResponseEntity<SituacaoFinanceira> situacao() {
+        return ResponseEntity.ok(situacaoService.analisar());
     }
 
     @PutMapping("/{id}")
