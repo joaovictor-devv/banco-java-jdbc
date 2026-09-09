e3871cbe0514e22e06a716320118186d5dacd786
João Victor Pereira de Lima <joaovictorjoaovictorpereira123@gmail.com>
2026-09-09 17:18:07 -0300

    refactor: remove renda extra do orcamento

--- src/main/java/com/joaovictor/dto/OrcamentoRequest.java
+++ src/main/java/com/joaovictor/dto/OrcamentoRequest.java
@@ -5,15 +5,11 @@ import java.math.BigDecimal;
 public class OrcamentoRequest {
 
     private BigDecimal rendaMensal;
-    private BigDecimal rendaExtra;
     private BigDecimal gastosMensais;
     private BigDecimal valorPlanejadoGuardar;
 
     public BigDecimal getRendaMensal() { return rendaMensal; }
     public void setRendaMensal(BigDecimal rendaMensal) { this.rendaMensal = rendaMensal; }
-
-    public BigDecimal getRendaExtra() { return rendaExtra; }
-    public void setRendaExtra(BigDecimal rendaExtra) { this.rendaExtra = rendaExtra; }
 
     public BigDecimal getGastosMensais() { return gastosMensais; }
     public void setGastosMensais(BigDecimal gastosMensais) { this.gastosMensais = gastosMensais; }
