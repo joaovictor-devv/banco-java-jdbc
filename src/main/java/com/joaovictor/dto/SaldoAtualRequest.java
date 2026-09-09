b1ea6197e1b25949de16b8e810f7cc20703d5acf
João Victor Pereira de Lima <joaovictorjoaovictorpereira123@gmail.com>
2026-09-09 16:51:01 -0300

    feat: adiciona request para atualizar saldo atual

---
+++ src/main/java/com/joaovictor/dto/SaldoAtualRequest.java
@@ -0,0 +1,11 @@
+package com.joaovictor.dto;
+
+import java.math.BigDecimal;
+
+public class SaldoAtualRequest {
+
+    private BigDecimal saldoAtual;
+
+    public BigDecimal getSaldoAtual() { return saldoAtual; }
+    public void setSaldoAtual(BigDecimal saldoAtual) { this.saldoAtual = saldoAtual; }
+}
