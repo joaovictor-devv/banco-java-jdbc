628391919c33ecde6da21ecfedb5d9136396ae51
João Victor Pereira de Lima <joaovictorjoaovictorpereira123@gmail.com>
2026-09-09 23:24:01 -0300

    feat: atualiza rotas para fluxo final do FinIA

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,24 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
-
 import Dashboard from "./pages/Dashboard";
-import Planejamento from "./pages/Planejamento";
-import Metas from "./pages/Metas";
 import Insights from "./pages/Insights";
+import Metas from "./pages/Metas";
 import Perfil from "./pages/Perfil";
-import RevisaoMensal from "./pages/RevisaoMensal";
+import Planejamento from "./pages/Planejamento";
+import Simulacoes from "./pages/Simulacoes";
 
 function App() {
   return (
     <BrowserRouter>
       <Sidebar />
-
-      <div className="pb-20 md:ml-[280px] md:pb-0">
+      <div className="pb-24 pt-16 md:ml-64 md:pb-0 md:pt-0">
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/planejamento" element={<Planejamento />} />
           <Route path="/metas" element={<Metas />} />
+          <Route path="/simulacoes" element={<Simulacoes />} />
           <Route path="/insights" element={<Insights />} />
           <Route path="/perfil" element={<Perfil />} />
-          <Route path="/revisao-mensal" element={<RevisaoMensal />} />
         </Routes>
       </div>
     </BrowserRouter>
